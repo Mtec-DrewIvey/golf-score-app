aeef07e756b9fe665943e4e106a0045062a38b0e
Drew Ivey <drew.ivey4543@stu.mtec.edu>
2024-02-29 13:03:32 -0700

    added js to grab available courses via the api and display the options to the user in a dropdown. started working on scorecard table Thu Feb 29 13:03:32 MST 2024

---
+++ js/main.js
@@ -0,0 +1,22 @@
+async function getAvailableCourses() {
+	const baseUrl =
+		"https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json";
+	const response = await fetch(baseUrl);
+	const data = await response.json();
+	return data;
+}
+
+async function displayCourses() {
+	const courses = await getAvailableCourses();
+
+	let courseOptionsHtml = "";
+
+	courses.forEach((course) => {
+		const courseId = course.id;
+		const courseName = course.name;
+		courseOptionsHtml += `<option value="${courseId}">${courseName}</option>`;
+	});
+	document.getElementById("course-select").innerHTML = courseOptionsHtml;
+}
+
+displayCourses();
